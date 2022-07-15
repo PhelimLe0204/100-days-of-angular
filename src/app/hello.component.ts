a696aac6c0b4e158f5b3fdbd226bbb2691455025
LE Thanh <lethanh@gmail.com>
2022-07-15 16:30:01 +0700

    Day 003

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,6 +1,20 @@
 import { Component } from "@angular/core";
 @Component({
   selector: "app-hello",
-  template: ` <h2>Hello there!</h2> `,
+  template: `
+    <h2>Chào đằng đó!</h2>
+    <h3>Tên: {{ user.name }}</h3>
+    <p>Tuổi: {{ user.age }}</p>
+    <button (click)="showInfo()">Nhấn cái!</button>
+  `,
 })
-export class HelloComponent { }
+export class HelloComponent {
+  user = {
+    name: "Thanh Le",
+    age: 24,
+  };
+
+  showInfo() {
+    alert("Bắt sự kiện onClick thành công!");
+  }
+}
